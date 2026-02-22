9a4745d2c759c2d2709dd3818d5397c8da95d806
D4rkNikita <justup121@gmail.com>
2026-02-22 19:57:49 +1000

    Добавлена анимация для меча

--- Assets/Scripts/Weapons/ActiveWeapon.cs
+++ Assets/Scripts/Weapons/ActiveWeapon.cs
@@ -11,8 +11,28 @@ public class ActiveWeapon : MonoBehaviour
         Instance = this;
     }
 
+    private void Update()
+    {
+        FollowMousePosition();
+    }
+
     public Sword GetActiveWeapon()
     {
         return sword;
+    }
+
+    private void FollowMousePosition()
+    {
+        Vector3 mousePos = GameInput.Instance.GetMousePosition();
+        Vector3 playerPos = Player.Instance.GetPlayerScreenPosition();
+
+        if (mousePos.x < playerPos.x)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
