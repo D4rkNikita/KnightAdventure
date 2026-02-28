4caf659b058167810216bd3462ad799fcf601efc
D4rkNikita <justup121@gmail.com>
2026-02-28 23:29:02 +1000

    gДобавили смерть врагу

--- Assets/Scripts/Skeleton/EnemyEntity.cs
+++ Assets/Scripts/Skeleton/EnemyEntity.cs
@@ -1,23 +1,35 @@
+using System;
 using System.Xml.Serialization;
 using UnityEngine;
 
 [RequireComponent(typeof(PolygonCollider2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(EnemyAI))]
+
 
 public class EnemyEntity : MonoBehaviour
 {
-    [SerializeField] private int _maxHealth;
+    [SerializeField] private EnemySO _enemySO;
+
+    public event EventHandler OnTakeHit;
+    public event EventHandler OnDeath;
+
     private int _currentHealth;
 
     private PolygonCollider2D _polygonCollider2D;
+    private BoxCollider2D _boxCollider2D;
+    private EnemyAI _enemyAI;
 
     private void Awake()
     {
         _polygonCollider2D = GetComponent<PolygonCollider2D>();
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+        _enemyAI = GetComponent<EnemyAI>();
     }
 
     public void Start()
     {
-        _currentHealth = _maxHealth;
+        _currentHealth = _enemySO.enemyHealth;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -28,7 +40,7 @@ public class EnemyEntity : MonoBehaviour
     public void TakeDamage(int damage)
     {
         _currentHealth -= damage;
-
+        OnTakeHit?.Invoke(this, EventArgs.Empty);
         DetectDeath();
     }
 
@@ -46,7 +58,12 @@ public class EnemyEntity : MonoBehaviour
     {
         if (_currentHealth <= 0)
         {
-            Destroy(gameObject);
+            _boxCollider2D.enabled = false;
+            _polygonCollider2D.enabled = false;
+
+            _enemyAI.SetDeathState();
+
+            OnDeath?.Invoke(this, EventArgs.Empty);
         }
 
     }
